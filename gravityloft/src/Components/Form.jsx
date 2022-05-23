85961af2d48fcec0efe76eb94bd3c666a5192538
Naveen Peddabuddi <nkrishvi1@gmail.com>
2022-05-23 19:39:06 +0530

    Navbar Partial complete

--- gravityloft/src/Components/Form.jsx
+++ gravityloft/src/Components/Form.jsx
@@ -4,13 +4,37 @@ import styled from 'styled-components'
 const Container = styled.div`
     width: 1920px;
     height: 801px;
-    top: 100px;
     border: 1px solid teal;
+    background-color: rgba(248, 248, 248, 1);
+    display: flex;
+    
+`
+const Leftdiv = styled.div`
+  width: 50%;
+  height: 40%;
+  border: 2px solid red;
+  margin: auto;
+`
+const Rightdiv = styled.div`
+  width: 50%;
+  height: 60%;
+  border: 2px solid blue;
+  margin: auto;
+`
+const LeftTop = styled.div`
+  width: 38.5%;
+  height: 30%;
+  border: 1px solid red;
 `
 const Form = () => {
   return (
     <Container>
-        <h1>Form</h1>
+        <Leftdiv>
+            <LeftTop></LeftTop>
+        </Leftdiv>
+        <Rightdiv>
+
+        </Rightdiv>
     </Container>
   )
 }
--- gravityloft/src/Components/Navbar.jsx
+++ gravityloft/src/Components/Navbar.jsx
@@ -1,16 +1,81 @@
 import React from 'react'
 import styled from 'styled-components'
-
+import logo from '../Assets/Images/logo.png'
 const Container = styled.div`
     width: 1920px;
     height: 100px;
     border: 1px solid blue;
+    display: flex;
+`
+const Rightdiv = styled.div`
+  width: 50%;
+  height: 50%;
+  /* border: 2px solid red; */
+  display: flex;
+  padding-left: 7%;
+  margin: auto;
+`
+const Leftdiv = styled.div`
+  width: 50%;
+  height: 50%;
+  /* border: 2px solid yellow; */
+  display: flex;
+  justify-content: flex-end;
+  padding-right: 7%;
+  margin: auto;
+`
+const Contact = styled.span`
+  font-family: 'DM Sans', sans-serif;
+  font-weight: 500;
+  font-size: 18px;
+  color:rgba(114, 116, 121, 1);
+  margin-right: 2.5%;
+  margin-top: 10px;
+`
+const Login = styled.button`
+  width: 130px;
+  height: 44px;
+  background: rgba(248, 248, 248, 1);
+  font-family: 'Work Sans', sans-serif;
+  color: rgba(87, 70, 236, 1);
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 21.11px;
+  border: 1.5px solid #5746EC;
+  border-radius: 7px;
+  margin-right: 1.5%;
+`
+const Register = styled.button`
+  width: 130px;
+  height: 44px;
+  background: rgba(87, 70, 236, 1);
+  font-family: 'Work Sans', sans-serif;
+  color:rgba(255, 255, 255, 1);
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 21.11px;
+  border-radius: 8px;
+  border: 0px;
+  box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.1);
 `
 const Navbar = () => {
   return (
       
     <Container>
-        Navbar
+        <Rightdiv>
+          <img src={logo} alt="" width='43px' height='42px'/>
+        </Rightdiv>
+        <Leftdiv>
+          <Contact>
+            Contact Us
+          </Contact>
+          <Login>
+            Login
+          </Login>
+          <Register>
+            Register
+          </Register>
+        </Leftdiv>
     </Container>
     
   )
